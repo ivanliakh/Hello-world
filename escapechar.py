2159ae9a945ab99a890b6fc1c81b4077b4d17043
ivanliakh <69403584+ivanliakh@users.noreply.github.com>
2020-08-08 17:51:08 -0400

    Add files via upload

---
+++ escapechar.py
@@ -0,0 +1,22 @@
+splitstring = "This string has been \nsplit over \nseveral\nlines"
+print(splitstring)
+
+tabbedString = "1\t2\t3\t4\t5\t6"
+print(tabbedString)
+
+print('The pet shop owner said "No, no \'e\'s uh,... he\'s resting".')
+#or
+print("The pet shop owner said \"No, no 'e's uh,... he's resting\".")
+#or
+print("""The pet shop owner said "No, no \
+'e's uh,... he's resting".""")
+
+anotherString = """This line has been \
+spleet over \
+several \
+lines"""
+
+print(anotherString)
+
+print("C:\\Users\\timbuchalka\\notes.txt")
+print(r"C:\Users\timbuchalka\notes.txt")
